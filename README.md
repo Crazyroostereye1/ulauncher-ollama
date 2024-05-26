e537d423228085da41ad2084dd6df53bd9faea99
Julian Beittel <julian@beittel.net>
2024-05-26 10:33:55 +0200

    Added stuff

---
+++ README.md
@@ -0,0 +1,10 @@
+# ULauncher Ollama
+Adds Ollama to your ULauncher
+
+## Requirements
+- python ollama
+- python httpx
+
+```bash
+pip install ollama httpx
+```
--- test.py
+++
@@ -1,8 +0,0 @@
-from ollama import Client
-import httpx
-
-client = Client(host="http://localhost:11434")
-try:
-    response = client.generate(model="orca-mini", prompt="Hello World!", stream=False)
-except httpx.ConnectError:
-    print("Failed to connect to Ollama")
